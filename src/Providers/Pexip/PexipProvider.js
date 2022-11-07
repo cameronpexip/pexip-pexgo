0527e383a3fe31326eb0645ce8ebf8de6ed4e9b5
Cameron McGuffie <cameron.mcguffie@pexip.com>
2022-11-07 14:49:11 +1000

    Enable presenation in mix

--- src/Providers/Pexip/PexipProvider.js
+++ src/Providers/Pexip/PexipProvider.js
@@ -205,6 +205,7 @@ export default function PexipProvider({ children }) {
           break;
         default:
           updatePinState('');
+          pexRTC.setPresentationInMix(true);
           pexRTC.connect();
       }
 
